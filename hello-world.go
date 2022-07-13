afd88534e0a2d854d0881cf772e5187294e35b92
OlaIsaac <OlaIsaac@ISAAC-KQ05F-MBP.local>
2022-07-13 14:08:54 -0300

    feat: variable declarations and package usage

--- hello-world.go
+++ hello-world.go
@@ -1,8 +1,51 @@
 package main
 
-func main() {
-	print("hello world")	
-}
+import (
+	"fmt"
+	"hello-go/example"
+	"net/http"
+)
 
-// go run
-// go build
+func main() {
+	const xpto int = 1
+
+	const (
+		x string = "aaaa"
+		y string = "bbbb"
+		z string = "cccc"
+	)
+
+	print(x)
+
+	res, _ := http.Get("https://google.com.br")
+
+	// if err != nil {
+	// 	panic("Houve um erro na requisição")
+	// }
+
+	fmt.Println(res.Body)
+
+	// var nome string = "hello world"
+	// var nome = "hello world"
+
+	a := "Gabriel"
+	b := 10
+	c := 10.44
+	d := false
+	e := 'a'
+	f := `
+		Gabriel
+		Gigante
+	`
+
+	print(a + "\n")
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%T \n", e)
+	fmt.Printf("%T \n", f)
+
+	example.PrintExample()
+}
