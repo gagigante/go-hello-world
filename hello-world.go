80f608daee4bedead24ace1a86d2244807c3b006
OlaIsaac <OlaIsaac@ISAAC-KQ05F-MBP.local>
2022-07-14 13:49:08 -0300

    feat: struct, interfaces and json

--- hello-world.go
+++ hello-world.go
@@ -1,14 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
+type vehicle interface {
+	start() string
+}
+
+type Car struct {
+	CarName string `json:"car"`
+	CarYear int    `json:"year"`
+}
+
+func (c Car) drive() {
+	print(c.CarName, " vrum vrum")
+}
+
+func (c Car) start() string {
+	return "**engine starts**"
+}
+
+func example(car vehicle) {
+	fmt.Println(car.start())
+}
+
 func main() {
-	mapx := make(map[string]int)
+	j := []byte(`{"car": "bmw", "year": 2020 }`)
 
-	mapx["gabriel"] = 1
-	mapx["gigante"] = 2
+	var car Car
 
-	fmt.Println(mapx["gabriel"])
+	json.Unmarshal(j, &car)
+
+	fmt.Println(car)
+
+	car1 := Car{
+		CarName: "Palio",
+		CarYear: 2008,
+	}
+
+	car1.drive()
+	println(car1.CarName)
+
+	result, _ := json.Marshal(car1)
+
+	fmt.Println(string(result))
+
+	example(car1)
 }
